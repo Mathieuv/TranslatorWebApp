53500d3597ea1e6d1aa93a92cbc7213ff601cc41
Mathieuv <mathieu_vermeire@hotmail.com>
2020-02-16 22:38:15 +0100

    Commit Frontend

---
+++ frontend/src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+const routeroptions = [
+  {path: '/', component: 'Translator', name: 'Translator'},
+  { path: '*', component: 'NotFound' }
+]
+
+
+const routes = routeroptions.map(route => {
+  return {
+    ...route,
+    component: () => import(`@/components/${route.component}.vue`)
+  }
+})
+
+Vue.use(Router)
+
+export default new Router({
+  routes,
+  mode: 'history'
+})
